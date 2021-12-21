56bdc8fce209af875a39e8d56fd315a9d017263f
Josh <joshua.mcswain@mecklenburgcountync.gov>
2021-12-21 17:45:50 -0500

    pushing

--- server/schema/typeDefs.js
+++ server/schema/typeDefs.js
@@ -2,10 +2,6 @@ const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
 
-type Query {
-  me: User
-}
-
 type Mutation {
   login(email: String!, password: String!): Auth
   addUser(username: String!, email: String!, password: String!): Auth
